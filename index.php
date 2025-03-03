fa576000103f32d0f31a2a08d2e828affd0a1448
T_TA3268 <ariamustofa@gmail.com>
2025-03-03 08:44:44 +0700

    fixing index

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ function displayNotification() {
             padding: 15px;
             background-color: #4CAF50;
             color: white;
+            margin-top: 15px;
             margin-bottom: 15px;
             border-radius: 4px;
             position: fixed;
