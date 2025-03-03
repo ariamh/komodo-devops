8537dc4000da80a828db24dd64e31ebe0a2ee526
aria hidayat <33375615+ariamh@users.noreply.github.com>
2025-03-03 09:58:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ function displayNotification() {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FitLife Personal Training - Layanan</title>
+    <title>FitLife Personal Training - Beranda</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
